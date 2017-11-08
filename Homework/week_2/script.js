6f39eb001b17934dd809d83dc94c9d146de08cb6
KevinVuongly <k.vuongly@gmail.com>
2017-11-08 12:57:00 +0100

    set html to root

--- Homework/week_2/script.js
+++ Homework/week_2/script.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 
-xhr.open('GET', '/data/KNMI.csv', true);
+xhr.open('GET', 'Homework/week_2/data/KNMI.csv', true);
 xhr.send();
 
 xhr.onreadystatechange = function(){
