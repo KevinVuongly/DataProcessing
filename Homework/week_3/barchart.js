5bdadae678e8dd91513b8710386a63742c5eb985
KevinVuongly <k.vuongly@gmail.com>
2017-11-15 21:29:50 +0100

    update week3

--- Homework/week_3/barchart.js
+++ Homework/week_3/barchart.js
@@ -1,45 +1,54 @@
+var title = d3.select('body').append('h1')
+              .text('Average temperature in De bilt(NL)');
+
 var dates = 366;
+var barWidth = 50;
 
 var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 25 * dates - margin.left - margin.right,
+    width = barWidth * dates - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select('body').append('svg').attr('class', 'chart')
+var svg = d3.select('body').append('svg').attr('class', 'chart');
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+var y = d3.scale.linear().range([height, 0]);
 
 var chart = d3.select(".chart")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
 d3.json('data/KNMI.json', function(data) {
   x.domain(data.map(function(d) { return d.date; }));
-  y.domain([d3.min(data, function(d) { return d.temperature / 10 - 2; }),
+  y.domain([d3.min(data, function(d) { return d.temperature / 10; }),
             d3.max(data, function(d) { return d.temperature / 10; })]);
 
-  var bar = chart.selectAll("g")
-                 .data(data)
-                 .enter().append("g")
-                 .attr("transform", function(d)
-                  { return "translate(" + x(d.date) + ",0)"; });
+  chart.append("g")
+       .attr("class", "x axis")
+       .attr("transform", "translate(0," + height + ")")
+       .call(xAxis);
 
-  bar.append("rect").attr("y", function(d) { return y(d.temperature / 10); })
-                    .attr("height", function(d)
-                    { return height - y(d.temperature / 10); })
-                    .attr("width", x.rangeBand());
+  chart.append("g")
+       .attr("class", "y axis")
+       .call(yAxis);
 
-  bar.append("text").attr("x", x.rangeBand() / 2)
-                    .attr("y", function(d)
-                    { return y(d.temperature / 10) + 3; })
-                    .attr("dy", ".75em")
-                    .text(function(d) { return d.temperature / 10; });
+  chart.selectAll(".bar")
+       .data(data)
+       .enter().append("rect")
+       .attr("class", "bar")
+       .attr("x", function(d) { return x(d.date); })
+       .attr("y", function(d) { return y(d.temperature / 10); })
+       .attr("width", barWidth - 5)
+       .attr("height", function(d) { return height - y(d.temperature / 10); });
+
 });
-
-function type(d) {
-  d.temperature = +d.temperature; // coerce to number
-  return d;
-}
