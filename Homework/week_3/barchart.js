fd79daf7b490ec10196e23390716306577fedc10
KevinVuongly <k.vuongly@gmail.com>
2017-11-16 21:13:30 +0100

    update barchart.js

--- Homework/week_3/barchart.js
+++ Homework/week_3/barchart.js
@@ -1,9 +1,17 @@
+// Name: Kevin Vuong
+// Student number: 10730141
+
+// create titles
 var title = d3.select('body').append('h1')
               .text('Average temperature in De bilt(NL)');
 
+// amount of dates in data
 var dates = 366;
+
+// width of bar in pixels
 var barWidth = 50;
 
+// width and height of barchart
 var margin = {top: 20, right: 30, bottom: 30, left: 40},
     width = barWidth * dates - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -11,10 +19,13 @@ var margin = {top: 20, right: 30, bottom: 30, left: 40},
 // add svg element
 d3.select('body').append('svg').attr('class', 'chart');
 
+// scale x
 var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
 
+// scale y
 var y = d3.scale.linear().range([height, 0]);
 
+// add chart to chart class
 var chart = d3.select('.chart')
               .attr('width', width + margin.left + margin.right)
               .attr('height', height + margin.top + margin.bottom)
@@ -22,42 +33,66 @@ var chart = d3.select('.chart')
               .attr('transform', 'translate(' + margin.left
                                               + ',' + margin.top + ')');
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient('bottom');
+// construct tooltip
+var tooltip = chart.append('g')
+  .attr('class', 'tooltip')
+  .style('display', 'none');
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient('left');
+// add atributes to tooltip
+tooltip.append('text')
+  .attr('x', 15)
+  .attr('dy', '1.2em')
+  .style('fontsize', '1.25em')
+  .attr('font-weight', 'bold');
 
+// add x-axis
+var xAxis = d3.svg.axis().scale(x).orient('bottom');
+
+// add y-axis
+var yAxis = d3.svg.axis().scale(y).orient('left');
+
+// get data, make barchart
 d3.json('data/KNMI.json', function(error, data) {
-  if (error) throw error;
+    // throw error when data is not found
+    if (error) throw error;
 
-  x.domain(data.map(function(d) { return d.date; }));
-  y.domain([d3.min(data, function(d) { return d.temperature / 10; }),
-            d3.max(data, function(d) { return d.temperature / 10; })]);
+    // construct domain and range
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([d3.min(data, function(d) { return d.temperature / 10; }),
+              d3.max(data, function(d) { return d.temperature / 10; })]);
 
-  chart.append('g')
-       .attr('class', 'axis axis-x')
-       .attr('transform', 'translate(0,' + height + ')')
-       .call(xAxis);
+    // add x-axis to barchart
+    chart.append('g')
+        .attr('class', 'axis axis-x')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
 
-  chart.append('g')
-       .attr('class', 'axis axis-y')
-       .call(yAxis);
+    // add y-axis to barchart
+    chart.append('g')
+        .attr('class', 'axis axis-y')
+        .call(yAxis);
 
-  chart.selectAll('.bar')
-       .data(data)
-       .enter().append('rect')
-       .attr('class', 'bar')
-       .attr('x', function(d) { return x(d.date); })
-       .attr('y', function(d) { return y(d.temperature / 10); })
-       .attr('width', barWidth - 5)
-       .attr('height', function(d) { return height - y(d.temperature / 10); });
-
+    // add each bar
+    chart.selectAll('.bar')
+        .data(data)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', function(d) { return x(d.date); })
+        .attr('y', function(d) { return y(d.temperature / 10); })
+        .attr('width', barWidth - 5)
+        .attr('height', function(d) { return height - y(d.temperature / 10); });
+        .on('mouseover', tooltip.style('display', null))
+        .on('mouseout', tooltip.style('display', 'none'))
+        .on('mousemove', function(d) {
+            var xPos = d3.mouse(this)[0] - 15;
+            var yPos = d3.mouse(this)[1] - 55;
+            tooltip.attr('transform', 'translate(' + xPos + ',' + yPos + ')');
+            tooltip.select('text').text('Temperature:' + ': ' + d.temperature / 10);
+        });
 });
 
+// URL-source of data
 d3.select('body').append('sup').text('data from KNMI')
-.on('click', function()
-{ window.open('http://projects.knmi.nl/klimatologie/daggegevens/selectie.cgi');
-});
+.on('click', function(){
+    window.open('http://projects.knmi.nl/klimatologie/daggegevens/selectie.cgi');
+})
